6d32f7bdfa04dc3983b49cf5473ccb4c0a3f4fa3
Aakash Limbad <aakashlimbad@Aakashs-MacBook-Pro.local>
2022-03-09 16:28:33 +0530

    initial change

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "demoamplify5f780cc6": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "api5302b222": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
